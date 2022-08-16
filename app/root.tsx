74d589a7b0c55e365a9f12e57a670a5068788793
汪心禾 <wangxinhe06@gmail.com>
2022-08-17 01:19:41 +0800

    Bootstrap starter template

--- app/root.tsx
+++ app/root.tsx
@@ -7,12 +7,15 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export const links = () => [{ rel: "stylesheet", href: bootstrap }];
 
 export default function App() {
   return (
