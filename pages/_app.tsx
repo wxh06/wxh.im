b252085e968c977160f86b745592de7f678bcf5f
汪心禾 <wangxinhe06@gmail.com>
2022-10-28 00:16:10 +0800

    Audience Analytics
    https://vercel.com/docs/concepts/analytics/audiences/quickstart

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import "../styles/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />; // eslint-disable-line react/jsx-props-no-spreading
+  return (
+    <>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
