fab16d846defcf29ab21313663e0a797adac0c8e
汪心禾 <wangxinhe06@gmail.com>
2023-07-11 00:38:14 +0800

    Fix navbar layout

--- src/app/(layout)/layout.tsx
+++ src/app/(layout)/layout.tsx
@@ -4,19 +4,23 @@ import NavLink from "./NavLink";
 export default function Layout({ children }: React.PropsWithChildren) {
   return (
     <div className="container mx-auto divide-y divide-gray-200 px-4 dark:divide-gray-700">
-      <nav className="my-4 flex justify-end space-x-3 text-lg">
-        <Link className="my-auto mr-auto text-2xl" href="/" rel="prefetch">
-          wxh06
-        </Link>
-        {[
-          { href: "/about", text: "About" },
-          { href: "/projects", text: "Projects" },
-          { href: "/links", text: "Links" },
-        ].map(({ href, text }) => (
-          <NavLink href={href} key={href}>
-            {text}
-          </NavLink>
-        ))}
+      <nav className="my-4 flex-wrap justify-between text-center sm:flex">
+        <div className="my-3 text-3xl sm:my-auto">
+          <Link href="/" rel="prefetch">
+            wxh06
+          </Link>
+        </div>
+        <div className="my-2 space-x-3 text-lg">
+          {[
+            { href: "/about", text: "About" },
+            { href: "/projects", text: "Projects" },
+            { href: "/links", text: "Links" },
+          ].map(({ href, text }) => (
+            <NavLink href={href} key={href}>
+              {text}
+            </NavLink>
+          ))}
+        </div>
       </nav>
       {children}
     </div>
