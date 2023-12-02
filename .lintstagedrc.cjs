d0dcf42fcf3a946766a95a4426e6e6ce2c32c94f
汪心禾 <wangxinhe06@gmail.com>
2023-12-03 03:15:15 +0800

    Fix white space

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -3,6 +3,6 @@ const prettier = "prettier --write";
 const prettierCheck = "prettier --check";
 
 module.exports = {
-  "*.{js,mjs,cjs,jsx,ts,tsx}": [eslint, prettierCheck],
+  "*.{js,mjs,cjs,ts,astro}": [eslint, prettierCheck],
   "*.{md,html,css,json,yml,yaml}": prettier,
 };
