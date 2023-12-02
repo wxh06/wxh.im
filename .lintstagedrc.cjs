34f50979f3dabc6d350f9d4d9e8bd1eb8bac17d5
汪心禾 <wangxinhe06@gmail.com>
2023-12-03 00:20:12 +0800

    Astro

--- .lintstagedrc.cjs
+++
@@ -1,8 +0,0 @@
-const eslint = "eslint --fix";
-const prettier = "prettier --write";
-const prettierCheck = "prettier --check";
-
-module.exports = {
-  "*.{js,mjs,cjs,jsx,ts,tsx}": [eslint, prettierCheck],
-  "*.{md,html,css,json,yml,yaml}": prettier,
-};
