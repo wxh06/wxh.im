a8e58d2c1307ee4ac0d97c968c0ee13dcc71698a
汪心禾 <wangxinhe06@gmail.com>
2023-12-03 14:40:08 +0800

    Fix project GitHub buttons

---
+++ src/components/project-github-buttons.ts
@@ -0,0 +1,21 @@
+import { render } from "github-buttons";
+
+const r = () =>
+  document.querySelectorAll("a[data-github-btn]").forEach((anchor) =>
+    render(
+      {
+        href: anchor.getAttribute("href")!,
+        "aria-label": anchor.getAttribute("aria-label")!,
+        "data-color-scheme":
+          "no-preference: light; light: light; dark: dark_dimmed;",
+        "data-icon": "octicon-star",
+        "data-size": "large",
+        "data-show-count": true,
+        "data-text": "Star",
+      },
+      (el) => anchor.parentNode?.replaceChild(el, anchor),
+    ),
+  );
+
+r();
+document.addEventListener("astro:after-swap", r);
