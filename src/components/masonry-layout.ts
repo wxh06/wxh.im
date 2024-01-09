5c24e6aa7dc6c70493c02496cec0bb8f9e46eaf4
汪心禾 <wangxinhe06@gmail.com>
2024-01-09 13:27:23 +0800

    Fix masonry error

--- src/components/masonry-layout.ts
+++ src/components/masonry-layout.ts
@@ -1,6 +1,6 @@
 import Masonry from "masonry-layout";
 
-document.addEventListener(
-  "astro:after-swap",
-  () => new Masonry("[data-masonry]"),
-);
+document.addEventListener("astro:after-swap", () => {
+  const e = document.querySelector("[data-masonry]");
+  if (e) new Masonry(e); // eslint-disable-line no-new
+});
