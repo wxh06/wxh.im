72d58e0c2f20998d85d62e516cd1a707fd8f6978
汪心禾 <wangxinhe06@gmail.com>
2024-01-09 19:41:19 +0800

    astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: "never",
-  integrations: [mdx(), tailwind()],
+  integrations: [mdx(), tailwind(), icon()],
   image: {
     remotePatterns: [
       {
